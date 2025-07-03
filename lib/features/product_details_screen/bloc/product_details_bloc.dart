217a7789af95c3b393c3fa767a7d6d6fd687a6ca
mostafiz <mostafizemon09@gmail.com>
2025-07-04 01:53:20 +0600

    create product details screen .

--- lib/features/product_details_screen/bloc/product_details_bloc.dart
+++ lib/features/product_details_screen/bloc/product_details_bloc.dart
@@ -7,7 +7,6 @@ part 'product_details_state.dart';
 class ProductDetailsBloc extends Bloc<ProductDetailsEvent, ProductDetailsState> {
   ProductDetailsBloc() : super(ProductDetailsInitial()) {
     on<ProductDetailsEvent>((event, emit) {
-      // TODO: implement event handler
     });
   }
 }
